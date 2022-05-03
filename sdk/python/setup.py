3729dd68704ffd820d6f6d9406ba3706b04f06b9
benjyiw <benjyiw@users.noreply.github.com>
2022-05-03 16:11:51 +0000

    chore: apply automatic generated SDK changes

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -54,6 +54,7 @@ setup(name='pulumi_catalystsquad_platform',
       install_requires=[
           'parver>=0.2.1',
           'pulumi>=3.0.0,<4.0.0',
+          'pulumi-aws>=5.0.0,<6.0.0',
           'semver>=2.8.1'
       ],
       zip_safe=False)
