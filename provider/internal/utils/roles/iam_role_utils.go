3e04ec90bcbc2ffe6aec0bebf5e496306fcc6e84
benjyiw <ben@wese.io>
2022-05-18 13:52:29 -0600

    fix: move util/helpers for iam roles

---
+++ provider/internal/utils/roles/iam_role_utils.go
@@ -0,0 +1,67 @@
+package roles
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/iam"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+// CreateIrsaAssumeRolePolicy creates an iam assume role policy for IRSA
+// https://docs.aws.amazon.com/eks/latest/userguide/create-service-account-iam-policy-and-role.html
+func CreateIrsaAssumeRolePolicy(oidcProviderArn pulumi.StringInput, oidcProviderURL pulumi.StringInput,
+	namespace string, serviceAccount string) pulumi.Output {
+
+	return pulumi.All(oidcProviderArn, oidcProviderURL).ApplyT(func(args []interface{}) (string, error) {
+		arn := args[0].(string)
+		provider := strings.TrimLeft(args[1].(string), "https://")
+		policyByteArray, err := json.Marshal(map[string]interface{}{
+			"Version": "2012-10-17",
+			"Statement": []map[string]interface{}{
+				{
+					"Action": "sts:AssumeRoleWithWebIdentity",
+					"Effect": "Allow",
+					"Principal": map[string]interface{}{
+						"Federated": arn,
+					},
+					"Condition": map[string]interface{}{
+						"StringEquals": map[string]string{
+							fmt.Sprintf("%s:sub", provider): fmt.Sprintf("system:serviceaccount:%s:%s", namespace, serviceAccount),
+						},
+					},
+				},
+			},
+		})
+		return string(policyByteArray), err
+	})
+}
+
+// CreateRoleWithPolicy creates an IAM policy and associates it to a new IAM role.
+func CreateRoleWithPolicy(ctx *pulumi.Context, resourceNamePrefix string, policyJSON string,
+	assumeRolePolicyJSON pulumi.Output, roleDescription string, parent pulumi.Resource) error {
+
+	policy, err := iam.NewPolicy(ctx, fmt.Sprintf("%s-policy", resourceNamePrefix), &iam.PolicyArgs{
+		Name:        pulumi.String(fmt.Sprintf("%s-policy", resourceNamePrefix)),
+		Description: pulumi.String(roleDescription),
+		Policy:      pulumi.String(policyJSON),
+	}, pulumi.Parent(parent))
+	if err != nil {
+		return err
+	}
+
+	role, err := iam.NewRole(ctx, fmt.Sprintf("%s-role", resourceNamePrefix), &iam.RoleArgs{
+		Name:             pulumi.String(fmt.Sprintf("%s-role", resourceNamePrefix)),
+		AssumeRolePolicy: assumeRolePolicyJSON,
+	}, pulumi.Parent(parent))
+	if err != nil {
+		return err
+	}
+
+	_, err = iam.NewRolePolicyAttachment(ctx, fmt.Sprintf("%s-role-policy-attachment", resourceNamePrefix), &iam.RolePolicyAttachmentArgs{
+		Role:      role,
+		PolicyArn: policy.Arn,
+	}, pulumi.Parent(parent))
+	return err
+}
